5ae803b3f46c8ade0771b6f3a98a983836ffabbe
Isaac Urbina <7547113+isaacurbina@users.noreply.github.com>
2025-05-15 15:38:42 -0500

    first commit

---
+++ NotesWatchOS Watch App/NotesWatchOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  NotesWatchOSApp.swift
+//  NotesWatchOS Watch App
+//
+//  Created by Isaac Urbina on 5/15/25.
+//
+
+import SwiftUI
+
+@main
+struct NotesWatchOS_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
