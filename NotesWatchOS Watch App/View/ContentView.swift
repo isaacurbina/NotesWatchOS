32f147793a27996aa0836c88b35c8c57c0a0c67c
Isaac Urbina <7547113+isaacurbina@users.noreply.github.com>
2025-05-16 16:17:24 -0500

    137. 4. Let's create a detail view for a note

--- NotesWatchOS Watch App/View/ContentView.swift
+++ NotesWatchOS Watch App/View/ContentView.swift
@@ -68,65 +68,68 @@ struct ContentView: View {
 	// MARK: - body
 	
 	var body: some View {
-		VStack {
-			HStack(alignment: .center, spacing: 6) {
-				TextField("Add New Note", text: $text)
-				
-				Button {
-					// 1. only run the button's action when the text field is not empty
-					guard text.isEmpty == false else { return }
+		NavigationView {
+			VStack {
+				HStack(alignment: .center, spacing: 6) {
+					TextField("Add New Note", text: $text)
 					
-					// 2. create a new note item and initialize it with the text value
-					let note = Note(id: UUID(), text: text)
-					
-					// 3. add the new note item to the notes array (append)
-					notes.append(note)
-					
-					// 4. make the text field empty
-					text = ""
-					
-					// 5. save the notes (function)
-					save()
-					
-				} label: {
-					Image(systemName: "plus.circle")
-						.font(.system(size: 42, weight: .semibold))
-				} // Button
-				.fixedSize()
-				.buttonStyle(PlainButtonStyle())
-				.foregroundColor(.accentColor)
-			} // HStack
-			
-			Spacer()
-			
-			if notes.count >= 1 {
-				List {
-					ForEach(notes) { note in
+					Button {
+						// 1. only run the button's action when the text field is not empty
+						guard text.isEmpty == false else { return }
 						
-						HStack {
-							Capsule()
-								.frame(width: 4)
-								.foregroundColor(.accentColor)
-							Text(note.text)
-						} // HStack
-					} // ForEach
-					.onDelete(perform: delete)
-					
-				} // List
-			} else {
+						// 2. create a new note item and initialize it with the text value
+						let note = Note(id: UUID(), text: text)
+						
+						// 3. add the new note item to the notes array (append)
+						notes.append(note)
+						
+						// 4. make the text field empty
+						text = ""
+						
+						// 5. save the notes (function)
+						save()
+						
+					} label: {
+						Image(systemName: "plus.circle")
+							.font(.system(size: 42, weight: .semibold))
+					} // Button
+					.fixedSize()
+					.buttonStyle(PlainButtonStyle())
+					.foregroundColor(.accentColor)
+				} // HStack
+				
 				Spacer()
-				Image(systemName: "note.text")
-					.resizable()
-					.scaledToFit()
-					.foregroundColor(.gray)
-					.opacity(0.25)
-					.padding(25)
-				Spacer()
-			}
-			
-		} // VStack
-		.navigationTitle("Notes")
-		.onAppear() { load() }
+				
+				if notes.count >= 1 {
+					List {
+						ForEach(0..<notes.count, id: \.self) { i in
+							NavigationLink(destination: DetailView(note: notes[i], count: notes.count, index: i)) {
+								HStack {
+									Capsule()
+										.frame(width: 4)
+										.foregroundColor(.accentColor)
+									Text(notes[i].text)
+								} // HStack
+							} // NavigationLink
+						} // ForEach
+						.onDelete(perform: delete)
+						
+					} // List
+				} else {
+					Spacer()
+					Image(systemName: "note.text")
+						.resizable()
+						.scaledToFit()
+						.foregroundColor(.gray)
+						.opacity(0.25)
+						.padding(25)
+					Spacer()
+				}
+				
+			} // VStack
+			.navigationTitle("Notes")
+			.onAppear() { load() }
+		} // NavigationView
 	}
 }
 
